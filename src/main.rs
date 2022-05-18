ed198a272eadf495b805801be100969e0d4076b4
Tulsonic <tulgar.dinc@gmail.com>
2022-05-18 11:48:59 +0200

    pwm0

--- src/main.rs
+++ src/main.rs
@@ -63,9 +63,9 @@ fn new_motor_test() {
     motor_latch_pin.set_high();
 
     let pwm0 =
-        pwm::Pwm::with_frequency(Channel::Pwm0, 200.0, 0.1, pwm::Polarity::Normal, true).unwrap();
+        pwm::Pwm::with_frequency(Channel::Pwm0, 200.0, 1.0, pwm::Polarity::Normal, true).unwrap();
     thread::sleep(Duration::from_millis(3000));
-    pwm0.set_duty_cycle(0.0).ok();
+    pwm0.disable().ok();
 }
 
 fn gpio_test() {
